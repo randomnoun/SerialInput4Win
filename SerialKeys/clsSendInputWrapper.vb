34fc0dd2c094a4d240ff68ddad7927ced588482c
Greg Knox <knoxg@randomnoun.com>
2013-01-18 05:17:04 +0000

    initial checkin of SerialKeys app (send input received on a COM port into the keyboard message queue of the foreground Windows window)

---
+++ SerialKeys/clsSendInputWrapper.vb
@@ -0,0 +1,148 @@
+﻿Imports System.Runtime.InteropServices
+
+' based on code downloaded from
+' http://social.msdn.microsoft.com/Forums/eu/vblanguage/thread/41d58def-87cb-41e5-93a4-2ad7d1fb7e0b
+' by knoxg on 2013-01-18
+
+
+Public Class clsSendInputWrapper
+
+    Private Declare Function GetForegroundWindow Lib "user32.dll" () As IntPtr
+
+    ' wrap the Windows SendInput API
+
+    Private Const VK_H As Short = 72
+    Private Const VK_E As Short = 69
+    Private Const VK_L As Short = 76
+    Private Const VK_O As Short = 79
+
+    Private Const KEYEVENTF_KEYUP As Integer = &H2
+    Private Const INPUT_MOUSE As Integer = 0
+    Private Const INPUT_KEYBOARD As Integer = 1
+    Private Const INPUT_HARDWARE As Integer = 2
+
+    Private Structure MOUSEINPUT
+        Public dx As Integer
+        Public dy As Integer
+        Public mouseData As Integer
+        Public dwFlags As Integer
+        Public time As Integer
+        Public dwExtraInfo As IntPtr
+    End Structure
+
+    Private Structure KEYBDINPUT
+        Public wVk As Short
+        Public wScan As Short
+        Public dwFlags As Integer
+        Public time As Integer
+        Public dwExtraInfo As IntPtr
+    End Structure
+
+    Private Structure HARDWAREINPUT
+        Public uMsg As Integer
+        Public wParamL As Short
+        Public wParamH As Short
+    End Structure
+
+    <StructLayout(LayoutKind.Explicit)> _
+    Private Structure INPUT
+        <FieldOffset(0)> _
+        Public type As Integer
+        <FieldOffset(4)> _
+        Public mi As MOUSEINPUT
+        <FieldOffset(4)> _
+        Public ki As KEYBDINPUT
+        <FieldOffset(4)> _
+        Public hi As HARDWAREINPUT
+    End Structure
+
+    Private Declare Function SendInput Lib "user32" (ByVal nInputs As Integer, ByVal pInputs() As INPUT, ByVal cbSize As Integer) As Integer
+    Private Declare Function AttachThreadInput Lib "user32" (ByVal idAttach As IntPtr, ByVal idAttachTo As IntPtr, ByVal fAttach As Boolean) As Boolean
+    Private Declare Function GetWindowThreadProcessId Lib "user32" (ByVal hWnd As IntPtr, ByVal lpwdProcessId As IntPtr) As IntPtr
+    Private Declare Function GetCurrentThreadId Lib "kernel32" () As IntPtr
+
+    Private Sub SendKey(ByVal bKey As Short)
+        Dim GInput(1) As INPUT
+
+        ' press the key
+        GInput(0).type = INPUT_KEYBOARD
+        GInput(0).ki.wVk = bKey
+        GInput(0).ki.dwFlags = 0
+
+        ' release the key
+        GInput(1).type = INPUT_KEYBOARD
+        GInput(1).ki.wVk = bKey
+        GInput(1).ki.dwFlags = KEYEVENTF_KEYUP
+
+        SendInput(2, GInput, Marshal.SizeOf(GetType(INPUT)))
+
+    End Sub
+
+    ' send keys to the current process' message queue
+    Public Function SendKeys(ByVal strText As String) As Boolean
+        Dim hwnd As IntPtr = GetForegroundWindow()
+
+        Dim threadId As IntPtr = GetWindowThreadProcessId(hwnd, IntPtr.Zero)
+        If threadId <> IntPtr.Zero Then
+            If AttachThreadInput(GetCurrentThreadId(), threadId, True) Then
+                Dim ch As String
+                Dim i As Integer
+                Dim k As Keys
+
+                'SendKey(VK_H)
+                'SendKey(VK_E)
+                'SendKey(VK_L)
+                'SendKey(VK_L)
+                'SendKey(VK_O)
+
+                Dim kc As KeysConverter = New KeysConverter()
+                For i = 0 To strText.Length - 1
+                    ch = strText.Substring(i, 1)
+                    Try
+                        If ch = "," Then
+                            SendKey(Keys.Oemcomma)
+                        ElseIf ch = " " Then
+                            SendKey(Keys.Space)
+                        Else
+                            ' k = CType(kc.ConvertFromString(ch), Keys) ' doesn't handle commas
+                            ' k = kc.getKey(ch)
+                            k = kc.ConvertFrom(ch)
+                            SendKey(k)
+                        End If
+                    Catch e As Exception
+                        ' forget about the rest
+                        AttachThreadInput(GetCurrentThreadId(), threadId, False)
+                        MsgBox(e.GetType.ToString() & ": " & e.Message)
+                        Return False
+                    End Try
+                Next
+
+                AttachThreadInput(GetCurrentThreadId(), threadId, False)
+                Return True
+            End If
+        End If
+        Return False
+    End Function
+
+    '    Sub Main()
+    '        Dim notepad As Process = Process.Start("notepad.exe")
+    '        If notepad.WaitForInputIdle() Then
+    '
+    '           Dim hNotePad As IntPtr = notepad.MainWindowHandle
+    '           Dim hNoteThread As IntPtr = GetWindowThreadProcessId(hNotePad, IntPtr.Zero)
+    '
+    '            If hNoteThread <> IntPtr.Zero Then
+    '                If AttachThreadInput(GetCurrentThreadId(), hNoteThread, True) Then
+    '                    SendKey(VK_H)
+    '                    SendKey(VK_E)
+    '                    SendKey(VK_L)
+    '                    SendKey(VK_L)
+    '                    SendKey(VK_O)
+    '
+    '                AttachThreadInput(GetCurrentThreadId(), hNotePad, False)
+    '            End If
+    '        End If
+    '    End If
+    'End Sub
+
+End Class
---
+++ SerialKeys/frmSerialKeys.vb
@@ -0,0 +1,277 @@
+﻿Imports System
+Imports System.IO.Ports
+Imports System.Reflection
+
+' TODO those incredibly syntactically verbose .NET comments
+
+Public Class frmSerialKeys
+
+    Private Declare Function GetForegroundWindow Lib "user32.dll" () As IntPtr
+
+    Dim WithEvents serialKeys As clsSerialKeys
+    Dim sendInputWrapper As New clsSendInputWrapper
+
+    Dim strBuffer As String = ""
+    Dim boolWasOpen As Boolean = False
+
+    Private Sub serialKeysDataReceivedHandler(ByVal strText As String) Handles serialKeys.DataRecieved
+        If Me.Handle = GetForegroundWindow Then
+            strBuffer = strBuffer & strText
+            txtTestInput.Font = New Font("Microsoft Sans Serif", 8, FontStyle.Regular)
+            txtTestInput.Text = strBuffer
+            txtTestInput.Focus()
+            txtTestInput.SelectionStart = txtTestInput.Text.Length
+            txtTestInput.SelectionLength = 0
+            txtTestInput.ScrollToCaret()
+        Else
+            ' add to input buffer
+            ' this probably belongs in the clsSerialKeys class
+            If sendInputWrapper.SendKeys(strText) Then
+                ' sent OK
+            Else
+                ' probably log this instead
+                MsgBox("Could not send text '" & strText & "'")
+            End If
+        End If
+    End Sub
+
+    Private Sub frmSerialKeys_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        serialKeys = New clsSerialKeys()
+
+        subSetInitialComboValues()
+        subSetFromCommandLineArgs()
+
+        txtTestInput.Text = "Set connection settings and hit apply to see test input"
+        txtTestInput.Font = New Font("Microsoft Sans Serif", 8, FontStyle.Italic)
+        subSetEnabled()
+
+        If chkEnabled.Checked Then
+            cmdApply_Click(Me, Nothing)
+        End If
+
+    End Sub
+
+    Private Sub subSetInitialComboValues()
+
+        Dim strPorts As String() = SerialPort.GetPortNames()
+        Dim strPort As String
+        For Each strPort In strPorts
+            cmbPort.Items.Add(strPort)
+        Next strPort
+
+        cmbParity.Items.Add(Parity.None.ToString())
+        cmbParity.Items.Add(Parity.Even.ToString())
+        cmbParity.Items.Add(Parity.Odd.ToString())
+        cmbParity.Items.Add(Parity.Mark.ToString())
+        cmbParity.Items.Add(Parity.Space.ToString())
+        cmbParity.SelectedIndex = 0
+
+        cmbDataBits.Items.Add(8)
+        cmbDataBits.Items.Add(7)
+        cmbDataBits.Items.Add(6)
+        cmbDataBits.Items.Add(5)
+        cmbDataBits.SelectedIndex = 0
+
+        ' cmbStopBits.Items.Add(StopBits.None.ToString()) ' illegal value
+        cmbStopBits.Items.Add(StopBits.One.ToString())
+        cmbStopBits.Items.Add(StopBits.OnePointFive.ToString())
+        cmbStopBits.Items.Add(StopBits.Two.ToString())
+        cmbStopBits.SelectedIndex = 0
+
+        cmbFlowControl.Items.Add(Handshake.None.ToString())
+        cmbFlowControl.Items.Add(Handshake.RequestToSendXOnXOff.ToString())
+        cmbFlowControl.Items.Add(Handshake.RequestToSend.ToString())
+        cmbFlowControl.Items.Add(Handshake.XOnXOff.ToString())
+        cmbFlowControl.SelectedIndex = 0
+    End Sub
+
+    Private Sub subSetFromCommandLineArgs()
+        Dim strArgs() As String, intCount As Integer
+        Dim boolSet As Boolean = False
+        strArgs = System.Environment.GetCommandLineArgs
+        ' TODO set to local vars first, then to form
+        ' (to ensure correct ordering)
+        ' TODO intCount checking on last arg
+        For intCount = 1 To UBound(strArgs)  ' 0th arg is filename
+            Select Case strArgs(intCount).ToLower
+                Case "-port", "-p"
+                    intCount = intCount + 1
+                    cmbPort.SelectedItem = strArgs(intCount)
+                    cmbPort_SelectedIndexChanged(Me, Nothing)
+                    boolSet = True
+                Case "-baud", "-b"
+                    intCount = intCount + 1
+                    cmbBaud.SelectedItem = strArgs(intCount)
+                    boolSet = True
+                Case "-data", "-d"
+                    intCount = intCount + 1
+                    cmbDataBits.SelectedItem = strArgs(intCount)
+                    boolSet = True
+                Case "-stop", "-s"
+                    intCount = intCount + 1
+                    cmbStopBits.SelectedItem = strArgs(intCount)
+                    boolSet = True
+                Case "-parity", "-r"
+                    intCount = intCount + 1
+                    cmbParity.SelectedItem = strArgs(intCount)
+                    boolSet = True
+                Case "-flowcontrol", "-flow", "-f"
+                    intCount = intCount + 1
+                    cmbFlowControl.SelectedItem = strArgs(intCount)
+                    boolSet = True
+                Case Else
+                    ' some error message
+            End Select
+        Next intCount
+
+        If boolSet Then chkEnabled.Checked = True
+
+    End Sub
+
+    Private Sub chkEnabled_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles chkEnabled.CheckedChanged
+        subSetEnabled()
+    End Sub
+
+    Private Sub subSetEnabled()
+        cmbPort.Enabled = chkEnabled.Checked
+        cmbBaud.Enabled = chkEnabled.Checked And cmbPort.SelectedItem IsNot Nothing
+        cmbDataBits.Enabled = chkEnabled.Checked And cmbPort.SelectedItem IsNot Nothing
+        cmbStopBits.Enabled = chkEnabled.Checked And cmbPort.SelectedItem IsNot Nothing
+        cmbParity.Enabled = chkEnabled.Checked And cmbPort.SelectedItem IsNot Nothing
+        cmbFlowControl.Enabled = chkEnabled.Checked And cmbPort.SelectedItem IsNot Nothing
+        'cmdTestInput.Enabled = chkEnabled.Checked And cmbPort.SelectedItem IsNot Nothing
+    End Sub
+
+
+    Private Sub cmdApply_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdApply.Click
+        ' MsgBox("Applying settings...")
+        Dim strError As String
+        serialKeys.Disconnect()
+        If cmbPort.Enabled And cmbPort.SelectedItem IsNot Nothing Then
+            strError = ""
+            If cmbPort.SelectedItem Is Nothing Then strError = "You must select a port"
+            If cmbBaud.SelectedItem Is Nothing Then strError = "You must select a baud"
+            If cmbParity.SelectedItem Is Nothing Then strError = "You must select a parity"
+            If cmbDataBits.SelectedItem Is Nothing Then strError = "You must select number of data bits"
+            If cmbStopBits.SelectedItem Is Nothing Then strError = "You must select number of stop bits"
+            If cmbFlowControl.SelectedItem Is Nothing Then strError = "You must select a flow control"
+
+            If strError <> "" Then
+                MsgBox(strError, MsgBoxStyle.Information, "Please check settings")
+                Exit Sub
+            End If
+
+
+            boolWasOpen = True
+            serialKeys.Connect(cmbPort.SelectedItem, _
+              Integer.Parse(cmbBaud.SelectedItem), _
+              CType([Enum].Parse(GetType(Parity), cmbParity.SelectedItem), Parity), _
+              Integer.Parse(cmbDataBits.SelectedItem), _
+              CType([Enum].Parse(GetType(StopBits), cmbStopBits.SelectedItem), StopBits), _
+              CType([Enum].Parse(GetType(Handshake), cmbFlowControl.SelectedItem), Handshake))
+            MsgBox("SerialKeys listening on " & cmbPort.SelectedItem)
+
+            txtTestInput.Text = "Waiting for input..."
+            txtTestInput.Font = New Font("Microsoft Sans Serif", 8, FontStyle.Italic)
+
+        Else
+            ' could retain buffer, but this is probably what the user wants
+            boolWasOpen = False
+            strBuffer = ""
+            MsgBox("SerialKeys disabled")
+
+            txtTestInput.Text = "Set connection settings and hit apply to see test input"
+            txtTestInput.Font = New Font("Microsoft Sans Serif", 8, FontStyle.Italic)
+        End If
+
+    End Sub
+
+    Private Sub cmdCancel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdCancel.Click
+        Application.Exit()
+
+    End Sub
+
+    ' no longer used
+    Private Sub cmdTestInput_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+        ' TODO: open up a separate window with text appearing as it's received
+
+        If Not boolWasOpen Then
+            MsgBox("You must click Apply to set connection settings before input can be tested")
+        Else
+            Dim strText As String = strBuffer
+            strBuffer = ""
+            MsgBox("Input received since last test: '" & strText & "'")
+        End If
+    End Sub
+
+    Private Sub frmSerialKeys_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        If serialKeys.IsOpen Then serialKeys.Disconnect()
+    End Sub
+
+    Private Sub cmbPort_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbPort.SelectedIndexChanged
+        If cmbPort.SelectedItem Is Nothing Then
+            Exit Sub
+        End If
+
+        ' update baud rates
+        ' via http://stackoverflow.com/questions/1165692/how-to-programatically-find-all-available-baudrates-in-c-sharp-serialport-class
+        Dim objPort As SerialPort
+        Dim p As Object
+        Dim strPortName As String
+        Dim bv As Int32
+
+        strPortName = cmbPort.SelectedItem
+        objPort = New SerialPort(strPortName)
+        Try
+            objPort.Open()
+        Catch ex As System.IO.IOException
+            MsgBox("There was an error opening the port: " & ex.Message)
+            cmbPort.SelectedItem = Nothing
+            subSetEnabled()
+            Exit Sub
+        Catch ex As System.UnauthorizedAccessException
+            MsgBox("Cannot open port: " & ex.Message)
+            cmbPort.SelectedItem = Nothing
+            subSetEnabled()
+            Exit Sub
+        Catch ex As Exception
+            MsgBox("Cannot open port: (" & ex.GetType().ToString() & ") " & ex.Message)
+            cmbPort.SelectedItem = Nothing
+            subSetEnabled()
+            Exit Sub
+        End Try
+
+        subSetEnabled()
+
+        'Message = "The port 'COM1' does not exist."
+        'Source = "System"
+        p = objPort.BaseStream.GetType().GetField("commProp", BindingFlags.Instance Or BindingFlags.NonPublic).GetValue(objPort.BaseStream)
+        bv = p.GetType().GetField("dwSettableBaud", BindingFlags.Instance Or BindingFlags.NonPublic Or BindingFlags.Public).GetValue(p)
+        objPort.Close()
+
+        ' from http://msdn.microsoft.com/en-us/library/aa363189%28VS.85%29.aspx
+        cmbBaud.Items.Clear()
+        Dim intSelected As Integer = 0
+        If (bv And &H1 > 0) Then cmbBaud.Items.Add("75")
+        If (bv And &H2 > 0) Then cmbBaud.Items.Add("110")
+        If (bv And &H4 > 0) Then cmbBaud.Items.Add("134.5") ' ?
+        If (bv And &H8 > 0) Then cmbBaud.Items.Add("150")
+        If (bv And &H10 > 0) Then cmbBaud.Items.Add("300")
+        If (bv And &H20 > 0) Then cmbBaud.Items.Add("600")
+        If (bv And &H40 > 0) Then cmbBaud.Items.Add("1200")
+        If (bv And &H80 > 0) Then cmbBaud.Items.Add("1800")
+        If (bv And &H100 > 0) Then cmbBaud.Items.Add("2400")
+        If (bv And &H200 > 0) Then cmbBaud.Items.Add("4800")
+        If (bv And &H400 > 0) Then cmbBaud.Items.Add("7200")
+        If (bv And &H800 > 0) Then cmbBaud.Items.Add("9600") : intSelected = cmbBaud.Items.Count - 1 ' default to this
+        If (bv And &H1000 > 0) Then cmbBaud.Items.Add("14400")
+        If (bv And &H2000 > 0) Then cmbBaud.Items.Add("19200")
+        If (bv And &H4000 > 0) Then cmbBaud.Items.Add("38400")
+        If (bv And &H8000 > 0) Then cmbBaud.Items.Add("56000")
+        If (bv And &H40000 > 0) Then cmbBaud.Items.Add("57600")
+        If (bv And &H20000 > 0) Then cmbBaud.Items.Add("115200")
+        If (bv And &H10000 > 0) Then cmbBaud.Items.Add("128000")
+        cmbBaud.SelectedIndex = intSelected
+    End Sub
+End Class
